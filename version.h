f7eb73b2976314e87e71286a2dfb918460b2e6a4
ztx <30760655@qq.com>
2025-11-11 17:22:59 +0800

    去掉法语和意大利语翻译 gui增加中文翻译 去掉版本号生成脚本 增加版本号头文件

---
+++ version.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define NSSM_VERSION _T("2.25.0")
+#define NSSM_VERSIONINFO 2,25,0,0
+#define NSSM_DATE _T("2025-11-11")
+#define NSSM_FILEFLAGS 0L
+#define NSSM_COPYRIGHT _T("Public Domain; Author Iain Patterson 2003-2025")
+
